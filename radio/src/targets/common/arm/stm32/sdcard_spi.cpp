5049670103d7463d450f6a1aee1ab95a0fd2b619
3djc <3djc@gh.com>
2024-03-09 17:48:01 +0100

    chore: cosmetics

--- radio/src/targets/common/arm/stm32/sdcard_spi.cpp
+++ radio/src/targets/common/arm/stm32/sdcard_spi.cpp
@@ -24,11 +24,10 @@
 #include "timers_driver.h"
 #include "delays_driver.h"
 
+#include "debug.h"
 #include "crc.h"
 
 #include <string.h>
-
-#include "debug.h"
 
 #define SD_SPI_CLK_400K (400000UL)
 
